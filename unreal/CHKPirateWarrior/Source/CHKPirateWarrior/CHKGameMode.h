0e3cd836b36c3b054a75e0f150f6472592374f5a
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-07-25 09:26:46 +0200

    feat(ue5): ajouter le game mode principal

---
+++ unreal/CHKPirateWarrior/Source/CHKPirateWarrior/CHKGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CHKGameMode.generated.h"
+
+UCLASS()
+class CHKPIRATEWARRIOR_API ACHKGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    ACHKGameMode();
+};
